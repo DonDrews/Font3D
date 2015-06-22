c7933ae54042e3438ebfa378e5d4be814c3c3f66
DonDrews <donsprogramming@gmail.com>
2015-06-22 15:07:12 -0700

    Added matrix code and mesh code to F3DModel

--- Font3D/src/font/data/F3DModel.java
+++ Font3D/src/font/data/F3DModel.java
@@ -2,6 +2,8 @@ package font.data;
 
 import java.nio.FloatBuffer;
 
+import com.jogamp.opengl.util.PMVMatrix;
+
 import font.material.F3DMaterial;
 import font.misc.F3DErrorManager;
 import font.vectors.F3DVector3;
@@ -18,6 +20,14 @@ public class F3DModel {
 	//matrix to be passed into shader
 	FloatBuffer matrix;
 	
+	//mesh for this matrix
+	F3DMesh mesh;
+	
+	public F3DModel(F3DMesh m)
+	{
+		this.mesh = m;
+		this.updateModelMatrix();
+	}
 	//getting and setting functions
 	public int getIndex()
 	{
@@ -69,5 +79,24 @@ public class F3DModel {
 	public void setMaterial(F3DMaterial m)
 	{
 		this.material = m;
+	}
+	
+	public void setMesh(F3DMesh m)
+	{
+		this.mesh = m;
+	}
+	
+	public void updateModelMatrix()
+	{
+		PMVMatrix matrixMaker = new PMVMatrix();
+		//rotate last
+		matrixMaker.glRotatef((float)Math.toDegrees(this.rotation), 0f, 1f, 0f);
+		//position middle
+		matrixMaker.glTranslatef(this.position.getX(), this.position.getY(), this.position.getZ());
+		//scale first
+		matrixMaker.glScalef(this.scale, this.scale, this.scale);
+		
+		//move changes to current matrix
+		this.matrix = matrixMaker.glGetMatrixf();
 	}
 }
